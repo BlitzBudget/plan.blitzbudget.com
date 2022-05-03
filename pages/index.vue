2cb0a4f130ed0e2570194135aac8bf381b51f75b
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-05-03 10:13:05 +0200

    Add default layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,196 @@
 <template>
-  <div class="wrapper">
-    <side-bar>
-      <template slot="links">
-        <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
-        <sidebar-link to="/UserProfile" name="User Profile" icon="ti-user"/>
-        <sidebar-link to="/TableList" name="Table List" icon="ti-view-list-alt"/>
-        <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
-        <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
-        <sidebar-link to="/maps" name="Map" icon="ti-map"/>
-        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
-      </template>
-      <mobile-menu>
-        <li class="nav-item">
-          <a class="nav-link">
-            <i class="ti-panel"></i>
-            <p>Stats</p>
-          </a>
-        </li>
-        <drop-down class="nav-item"
-                   title="5 Notifications"
-                   title-classes="nav-link"
-                   icon="ti-bell">
-          <a class="dropdown-item">Notification 1</a>
-          <a class="dropdown-item">Notification 2</a>
-          <a class="dropdown-item">Notification 3</a>
-          <a class="dropdown-item">Notification 4</a>
-          <a class="dropdown-item">Another notification</a>
-        </drop-down>
-        <li class="nav-item">
-          <a class="nav-link">
-            <i class="ti-settings"></i>
-            <p>Settings</p>
-          </a>
-        </li>
-        <li class="divider"></li>
-      </mobile-menu>
-    </side-bar>
-    <div class="main-panel">
-      <top-navbar></top-navbar>
+  <div>
 
-      <dashboard-content @click.native="toggleSidebar">
-
-      </dashboard-content>
-
-      <content-footer></content-footer>
+    <!--Stats cards-->
+    <div class="row">
+      <div class="col-md-6 col-xl-3" v-for="stats in statsCards" :key="stats.title">
+        <stats-card>
+          <div class="icon-big text-center" :class="`icon-${stats.type}`" slot="header">
+            <i :class="stats.icon"></i>
+          </div>
+          <div class="numbers" slot="content">
+            <p>{{stats.title}}</p>
+            {{stats.value}}
+          </div>
+          <div class="stats" slot="footer">
+            <i :class="stats.footerIcon"></i> {{stats.footerText}}
+          </div>
+        </stats-card>
+      </div>
     </div>
+
+    <!--Charts-->
+    <div class="row">
+
+      <div class="col-12">
+        <chart-card title="Users behavior"
+                    sub-title="24 Hours performance"
+                    :chart-data="usersChart.data"
+                    :chart-options="usersChart.options">
+          <span slot="footer">
+            <i class="ti-reload"></i> Updated 3 minutes ago
+          </span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Open
+            <i class="fa fa-circle text-danger"></i> Click
+            <i class="fa fa-circle text-warning"></i> Click Second Time
+          </div>
+        </chart-card>
+      </div>
+
+      <div class="col-md-6 col-12">
+        <chart-card title="Email Statistics"
+                    sub-title="Last campaign performance"
+                    :chart-data="preferencesChart.data"
+                    chart-type="Pie">
+          <span slot="footer">
+            <i class="ti-timer"></i> Campaign set 2 days ago</span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Open
+            <i class="fa fa-circle text-danger"></i> Bounce
+            <i class="fa fa-circle text-warning"></i> Unsubscribe
+          </div>
+        </chart-card>
+      </div>
+
+      <div class="col-md-6 col-12">
+        <chart-card title="2015 Sales"
+                    sub-title="All products including Taxes"
+                    :chart-data="activityChart.data"
+                    :chart-options="activityChart.options">
+          <span slot="footer">
+            <i class="ti-check"></i> Data information certified
+          </span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Tesla Model S
+            <i class="fa fa-circle text-warning"></i> BMW 5 Series
+          </div>
+        </chart-card>
+      </div>
+
+    </div>
+
   </div>
 </template>
-<style lang="scss">
-</style>
 <script>
-import TopNavbar from "@/layouts/dashboard/TopNavbar.vue";
-import ContentFooter from "@/layouts/dashboard/ContentFooter.vue";
-import DashboardContent from "@/layouts/dashboard/Content.vue";
-import MobileMenu from "@/layouts/dashboard/MobileMenu";
-import SideBar from "@/components/SidebarPlugin/SideBar";
-import SidebarLink from "@/components/SidebarPlugin/SidebarLink";
-import DropDown from "@/components/Dropdown";
-
+import { StatsCard, ChartCard } from "@/components/index";
+import Chartist from 'chartist';
 export default {
   components: {
-    TopNavbar,
-    ContentFooter,
-    DashboardContent,
-    MobileMenu,
-    SideBar,
-    SidebarLink,
-    DropDown
+    StatsCard,
+    ChartCard
   },
-  methods: {
-    toggleSidebar() {
-      if (this.$sidebar.showSidebar) {
-        this.$sidebar.displaySidebar(false);
+  /**
+   * Chart data used to render stats, charts. Should be replaced with server data
+   */
+  data() {
+    return {
+      statsCards: [
+        {
+          type: "warning",
+          icon: "ti-server",
+          title: "Capacity",
+          value: "105GB",
+          footerText: "Updated now",
+          footerIcon: "ti-reload"
+        },
+        {
+          type: "success",
+          icon: "ti-wallet",
+          title: "Revenue",
+          value: "$1,345",
+          footerText: "Last day",
+          footerIcon: "ti-calendar"
+        },
+        {
+          type: "danger",
+          icon: "ti-pulse",
+          title: "Errors",
+          value: "23",
+          footerText: "In the last hour",
+          footerIcon: "ti-timer"
+        },
+        {
+          type: "info",
+          icon: "ti-twitter-alt",
+          title: "Followers",
+          value: "+45",
+          footerText: "Updated now",
+          footerIcon: "ti-reload"
+        }
+      ],
+      usersChart: {
+        data: {
+          labels: [
+            "9:00AM",
+            "12:00AM",
+            "3:00PM",
+            "6:00PM",
+            "9:00PM",
+            "12:00PM",
+            "3:00AM",
+            "6:00AM"
+          ],
+          series: [
+            [287, 385, 490, 562, 594, 626, 698, 895, 952],
+            [67, 152, 193, 240, 387, 435, 535, 642, 744],
+            [23, 113, 67, 108, 190, 239, 307, 410, 410]
+          ]
+        },
+        options: {
+          low: 0,
+          high: 1000,
+          showArea: true,
+          height: "245px",
+          axisX: {
+            showGrid: false
+          },
+          lineSmooth: Chartist.Interpolation.simple({
+            divisor: 3
+          }),
+          showLine: true,
+          showPoint: false
+        }
+      },
+      activityChart: {
+        data: {
+          labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "Mai",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"
+          ],
+          series: [
+            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
+            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
+          ]
+        },
+        options: {
+          seriesBarDistance: 10,
+          axisX: {
+            showGrid: false
+          },
+          height: "245px"
+        }
+      },
+      preferencesChart: {
+        data: {
+          labels: ["62%", "32%", "6%"],
+          series: [62, 32, 6]
+        },
+        options: {}
       }
-    }
+    };
   }
 };
 </script>
+<style>
+</style>
